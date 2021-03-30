71741cdf27d4c916a364de69a80fe684f8f63fa8
mssffct <herooftheday82@gmail.com>
2021-03-30 15:31:01 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('body')/*.css('background-image', 'url(img/planet.png)')*/.css('height', '100%').css('position', 'absolute');
+	$('body')/*.css('background-repeat', 'no-repeat')*//*.css('background-position', '310px 150px')*/;
+	$('.navbar-default').css('background-color', 'white').css('width', '100%');
+	$('.nav-pills li a').css('color', 'white');
+	$('#background').css('background-image', 'url(img/planet.png)').css('height', '100%');
+	$('#background').css('background-repeat', 'no-repeat').css('background-position', 'center');
+	/*$('.row #block').css('background-color', 'yellow');*/
+	$('.navbar').css('background-color', '#e3f2fd');
+	$('a').css('color', 'black');
+});
